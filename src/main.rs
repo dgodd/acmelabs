e3672cea53cd72c208182709954c692ba11e6e1a
Dave Goddard <dave@goddard.id.au>
2018-11-13 19:45:29 -0400

    obtain ip address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize, Debug)]
+struct Ipify {
+    ip: String,
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut response = reqwest::get("https://api.ipify.org?format=json")?;
+    let ipify: Ipify = response.json()?;
+    println!("{:?}", ipify);
+    Ok(())
 }
