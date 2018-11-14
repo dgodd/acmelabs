31913ab0d893aa93e43d19e9f20856cfd73367e1
Dave Goddard <dave@goddard.id.au>
2018-11-13 20:29:52 -0400

    rustup run nightly rustfmt src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,27 @@ struct ListDnsRecords {
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
-let client = reqwest::Client::new();
+    let client = reqwest::Client::new();
     let mut response = client.get("https://api.ipify.org?format=json").send()?;
     let ipify: Ipify = response.json()?;
     println!("{:?}", ipify);
 
-    let mut response = client.get(&format!("https://api.digitalocean.com/v2/domains/{}/records", "goddard.id.au"))
-        .header("Authorization", format!("Bearer {}",std::env::var("DIGITALOCEAN_TOKEN")?)).send()?;
+    let mut response = client
+        .get(&format!(
+            "https://api.digitalocean.com/v2/domains/{}/records",
+            "goddard.id.au"
+        ))
+        .header(
+            "Authorization",
+            format!("Bearer {}", std::env::var("DIGITALOCEAN_TOKEN")?),
+        )
+        .send()?;
     let records: ListDnsRecords = response.json()?;
-    let record = records.domain_records.iter().find(|&r| r.name == "acmelabs").ok_or("acmelabs not found")?;
+    let record = records
+        .domain_records
+        .iter()
+        .find(|&r| r.name == "acmelabs")
+        .ok_or("acmelabs not found")?;
     println!("{:?}", record);
 
     if record.data == ipify.ip {
@@ -37,10 +49,21 @@ let client = reqwest::Client::new();
         let mut map = std::collections::HashMap::new();
         map.insert("data", ipify.ip.to_owned());
 
-        let mut response = client.put(&format!("https://api.digitalocean.com/v2/domains/{}/records/{}", "goddard.id.au", record.id))
+        let mut response = client
+            .put(&format!(
+                "https://api.digitalocean.com/v2/domains/{}/records/{}",
+                "goddard.id.au", record.id
+            ))
             .json(&map)
-            .header("Authorization", format!("Bearer {}",std::env::var("DIGITALOCEAN_TOKEN")?)).send()?;
-        println!("acmelabs.goddard.id.au was updated from {} to {}", record.data, ipify.ip)
+            .header(
+                "Authorization",
+                format!("Bearer {}", std::env::var("DIGITALOCEAN_TOKEN")?),
+            )
+            .send()?;
+        println!(
+            "acmelabs.goddard.id.au was updated from {} to {}",
+            record.data, ipify.ip
+        )
     }
 
     Ok(())
